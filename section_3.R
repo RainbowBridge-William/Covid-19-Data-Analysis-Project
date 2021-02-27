6a946618a9a5b05bb488aaaa4669663f3c0be29f
William <swang45@uw.edu>
2021-02-27 13:46:08 +0800

    Updated bug fix in section_2

--- section_3.R
+++ section_3.R
@@ -88,9 +88,9 @@ vaccine_vs_rate_case_scatter_plot <- ggplot(data = vaccine_vs_rate_case, mapping
                                               aes(x = Ratio_Doses_Administered, 
                                                   y = case_rate_most_recent)) + 
   geom_point(size = 1) +
+  geom_smooth(mapping = aes(x = Ratio_Doses_Administered, y = case_rate_most_recent), method = "lm") +
   scale_x_continuous(labels = scales::percent) +
   scale_y_continuous(labels = scales::percent) +
   labs(title = "The Rate of Cases vs Percent of Vaccination population in each state\n   on most recent date", 
        x = "Percent of Vaccination Population", y = "COVID Cases Rate")
-
 
