44ff9a515018da792b5ba17020548e7fb52d8023
Kyler Smith <kyler127@uw.edu>
2021-03-12 20:06:30 -0800

    Saved State_stay_at_home_order_data.csv with a different encoding to fix bug

--- covid_app_UI.R
+++ covid_app_UI.R
@@ -181,24 +181,17 @@ vaccination_data_panel <- tabPanel("Vaccination Data",
                                    ))
 
 
-#*********************************************************
+#******************* Mask use **********************
 
 mask_data_panel <- tabPanel("Mask Use Data")
 
-stay_home_order_time_slider <- sliderInput(inputId = "max_date",
-                                           label = h5("Date Range: "),
-                                           min = min(state_stay_at_home_order_data_df$Order.date), 
-                                           max = max(state_stay_at_home_order_data_df$Order.date) + 90,
-                                           value = min(state_stay_at_home_order_data_df$Order.date) + 30
-                                           )
-
-stay_home_order_filter_buttons <- radioButtons(inputId = "filter", 
-                                                  label = h5("Time vs. : "),
-                                                  choices = c("Number of New Cases", 
-                                                              "Number of Contagious Cases",
-                                                              "State Contagious Population Percentage",
-                                                              "Percent Change In Contagious Cases")
-                                               )
+mask_survey_answers <- c(
+   "Never" = "NEVER",
+   "Rarely" = "RARELY",
+   "Sometimes" = "SOMETIMES",
+   "Frequently" = "FREQUENTLY",
+   "Always" = "ALWAYS"
+)
 
 mask_data_panel <- tabPanel(
    "Mask Use Data",
@@ -229,6 +222,7 @@ mask_data_panel <- tabPanel(
    )
 )
 
+#**********************************************
 
 stay_home_order_time_slider <- sliderInput(inputId = "num_days_since_order",
                                               label = h5("Days Since Order: "),
